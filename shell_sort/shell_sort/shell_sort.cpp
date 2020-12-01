7f82b896622c65f134f2d589f15aa438bbc15f45
slava <osslav@mail.com>
2020-12-01 11:03:59 +0700

    add heap sorting

--- shell_sort/shell_sort/shell_sort.cpp
+++ shell_sort/shell_sort/shell_sort.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <ctime>
-using namespace std;
 
 /*
 Отчет хранится в    shell_sort_lab\shell_sort\shell_sort\Result.txt
@@ -35,18 +34,23 @@ public:
 	~Array() { delete array_; };
 
 	void randArray(int minNumber = DEFAULT_MIN_NUMBER, int maxNumber = DEFAULT_MAX_NUMBER);
-	friend ofstream& operator <<(ofstream& file, Array& Mas);
-	friend ifstream& operator >>(ifstream& file, Array& Mas);
+	friend std::ofstream& operator <<(std::ofstream& file, Array& Mas);
+	friend std::ifstream& operator >>(std::ifstream& file, Array& Mas);
 	void output();
 
 	int sortingShell(int typeStepArray, bool outputStep = false);
 	void sortingDirectInsert(int firstInd = 0, int step = 1);
+
+	int sortingHeap();
+	void sift(int index, int n = -1);
+	void siftWithoutClipboard(int i, int n = -1);
 
 	bool checkSorting();
 
 	void operator =(const Array& copy);
 
 	int getCount() { return count_; };
+
 };
 
 Array::Array(const int* copy, int count)
@@ -66,8 +70,8 @@ Array::Array(const Array& copy)
 
 void Array::output()
 {
-	for (int i = 0; i < count_; i++) cout << array_[i] << "  ";
-	cout << '\n';
+	for (int i = 0; i < count_; i++) std::cout << array_[i] << "  ";
+	std::cout << '\n';
 }
 
 
@@ -76,7 +80,7 @@ void Array::randArray(int minNumber, int maxNumber)
 	for (int i = 0; i < count_; i++) array_[i] = rand() % (maxNumber + 1) + minNumber;
 }
 
-ofstream& operator <<(ofstream& file, Array& Mas)
+std::ofstream& operator <<(std::ofstream& file, Array& Mas)
 {
 	file << Mas.count_;
 	for (int i = 0; i < Mas.count_; i++) file << ' ' << Mas.array_[i];
@@ -84,7 +88,7 @@ ofstream& operator <<(ofstream& file, Array& Mas)
 	return file;
 }
 
-ifstream& operator >>(ifstream& file, Array& Mas)
+std::ifstream& operator >>(std::ifstream& file, Array& Mas)
 {
 	int oldCount = Mas.count_;
 	file >> Mas.count_;
@@ -128,7 +132,7 @@ Array* Array::createStepArr1_(bool outputStep)                 // h_i = h_(i-1) 
 	
 	if (outputStep)
 	{
-		cout << "Step array: ";
+		std::cout << "Step array: ";
 		result->output();
 	}
 	return result;
@@ -151,7 +155,7 @@ Array* Array::createStepArr2_(bool outputStep)          // h_i = 2^i - 1
 	
 	if (outputStep)
 	{
-		cout << "Step array: ";
+		std::cout << "Step array: ";
 		result->output();
 	}
 	return result;
@@ -174,7 +178,7 @@ Array* Array::createStepArr3_(bool outputStep)           // h_(i+1) = 3*h_i +1
 
 	if (outputStep)
 	{
-		cout << "Step array: ";
+		std::cout << "Step array: ";
 		result->output();
 	}
 	return result;
@@ -212,6 +216,108 @@ int Array::sortingShell(int typeStepArray, bool outputStep)
 	return endTime - startTime;
 }
 
+
+void Array::siftWithoutClipboard(int i, int n)
+{
+	
+	if (n == -1) n = count_;
+	else
+	{
+		if (i < 0) throw "Error in sift function. index < 0.\n";
+		if (i >= count_) throw "Error in sift function. index >= count_.\n";
+	}
+
+	
+	int j = 2 * i + 1;
+
+	while (j < n)
+	{
+		if (j + 1 < n)									//проверяем существование правого потомка
+			if (array_[j + 1] > array_[j]) j++;			//ищем максимум среди потомков
+
+		if (array_[j] <= array_[i]) return;				//поверка выполнения условия дерева
+
+		int c = array_[i];								//array_[i] <=> array_[j]
+		array_[i] = array_[j];							
+		array_[j] = c;
+
+		i = j;											//идем дальше по дереву
+		j = 2 * i + 1;
+
+	}
+}
+
+void Array::sift(int i, int n)
+{
+
+	if (n == -1) n = count_;
+	else
+	{
+		if (i < 0) throw "Error in sift function. index < 0.\n";
+		if (i >= count_) throw "Error in sift function. index >= count_.\n";
+	}
+
+
+	int j = 2 * i + 1;
+	int clipboard = array_[i];							//буфер обмена
+	bool flag = true;									//флаг выхода из цикла
+
+	while ((j < n) && flag)
+	{
+		if (j + 1 < n)									//проверяем существование правого потомка
+			if (array_[j + 1] > array_[j]) j++;			//ищем максимум среди потомков
+
+		if (array_[j] <= clipboard) flag = false;		//поверка выполнения условия дерева
+		else
+		{
+			array_[i] = array_[j];						//сдвигаем элемент j ниже по дереву
+
+			i = j;										//идем дальше по дереву
+			j = 2 * i + 1;
+		}
+	}
+
+	array_[i] = clipboard;
+}
+
+int Array::sortingHeap()
+{
+	if (count_ < 2) return 0;							// если в массиве 1 или меньше элементов, он не требует сортировки
+
+	int startTime = clock();
+
+	int lastElemWithDescendant = 1;
+
+	while (lastElemWithDescendant < count_) lastElemWithDescendant = lastElemWithDescendant * 2 + 1;		//ищем предпоследний ярус дерева
+	lastElemWithDescendant = (lastElemWithDescendant - 1) / 2;
+
+	while (lastElemWithDescendant * 2 + 1 < count_) lastElemWithDescendant--;				//ищем последний элемент, у которого есть потомки
+
+	
+	int index = lastElemWithDescendant;
+	while (index >= 0)									//формируем первоначальное сортирующее дерево
+	{
+		sift(index);
+		index--;
+	}
+
+
+	int n = count_;
+
+	while (n > 1)
+	{
+		int c = array_[n - 1];								//array_[0] <=> array_[n - 1]
+		array_[n - 1] = array_[0];
+		array_[0] = c;
+		
+		n--;
+		sift(0, n);											//"просеиваем" 0 элемент
+	}
+
+	int endTime = clock();
+	return endTime - startTime;
+}
+
 bool Array::checkSorting()
 {
 	for (int i = 1; i < count_; i++)
@@ -232,7 +338,7 @@ void Array::operator =(const Array& copy)
 
 void createArrays(const char* fileName)
 {
-	ofstream fileArray(fileName);
+	std::ofstream fileArray(fileName);
 	
 	for (int count = 10000; count <= 1000000; count *= 10)
 	{
@@ -248,8 +354,8 @@ void createArrays(const char* fileName)
 
 void analysisShellSorting(const char* fileArraysName, const char* fileResultName)
 {
-	ifstream fileArray(fileArraysName);
-	ofstream fileResult(fileResultName);
+	std::ifstream fileArray(fileArraysName);
+	std::ofstream fileResult(fileResultName);
 	
 	int lastCount = -1;
 	int range = 10;
@@ -302,6 +408,57 @@ void analysisShellSorting(const char* fileArraysName, const char* fileResultName
 	}
 }
 
+
+void analysisHeapSorting(const char* fileArraysName, const char* fileResultName)
+{
+	std::ifstream fileArray(fileArraysName);
+	std::ofstream fileResult(fileResultName);
+
+	int lastCount = -1;
+	int range = 10;
+
+	fileResult << "Heap sorting algorithm\n";
+
+
+	Array A;
+	fileArray >> A;
+	while (fileArray)
+	{
+		if (lastCount != A.getCount()) fileResult << "Arrays with " << A.getCount() << " elements:\n";
+		lastCount = A.getCount();
+
+		fileResult << "    With range from " << -range << " to " << range << "\n";
+
+		if (range == 100000) range = 10;
+		else range *= 100;
+
+
+			fileResult << "            Heap sorting  time: ";
+
+			int summTime = 0;
+			int countRepeatSorts = 3;
+			for (int j = 0; j < countRepeatSorts; j++)
+			{
+				Array forSorting(A);
+				int sortingTime = forSorting.sortingHeap();
+
+				fileResult << sortingTime << "ms  ";
+				summTime += sortingTime;
+
+				if (!forSorting.checkSorting())
+				{
+					fileResult << "Error! Array was not sorted\n";
+					fileResult.close();
+					fileArray.close();
+					throw "Error! Array was not sorted\n";
+				}
+			}
+			fileResult << "     Average: " << summTime / countRepeatSorts << '\n';
+
+		fileArray >> A;
+	}
+}
+
 int main()
 {
 	srand(time(NULL));
@@ -310,14 +467,15 @@ int main()
 	{
 		//createArrays("Array.txt");
 
-		analysisShellSorting("Array.txt", "Result.txt");
-	
+		//analysisShellSorting("Array.txt", "ResultShellSortingTest.txt");
+
+		analysisHeapSorting("Array.txt", "ResultHeapSorting.txt");
 	}
 	
 	
 	catch (const char* errorString)
 	{
-		for (int i = 0; errorString[i] != '\0'; i++) cout << errorString[i];
+		for (int i = 0; errorString[i] != '\0'; i++) std::cout << errorString[i];
 		return 1;
 	}
 	
